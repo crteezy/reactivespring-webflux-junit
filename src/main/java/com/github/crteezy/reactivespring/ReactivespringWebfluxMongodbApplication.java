91a9c3e9ff9137d6eac4909fd1cc133ad9e5def6
Chamithra Thenuwara <chamithra@gmail.com>
2020-02-13 21:04:12 +0530

    intial file commit

---
+++ src/main/java/com/github/crteezy/reactivespring/ReactivespringWebfluxMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.github.crteezy.reactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivespringWebfluxMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivespringWebfluxMongodbApplication.class, args);
+	}
+
+}
